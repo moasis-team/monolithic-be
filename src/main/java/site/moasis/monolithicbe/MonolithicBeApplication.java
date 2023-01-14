bd348c71b86d6e3cbe73d01dbc1f68f54852e6ad
Hott-J <jhj13062004@naver.com>
2023-01-14 19:38:40 +0900

    init
    프로젝트 초기 설정 진행

---
+++ src/main/java/site/moasis/monolithicbe/MonolithicBeApplication.java
@@ -0,0 +1,13 @@
+package site.moasis.monolithicbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithicBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/moasis/monolithicbe/MonolithicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.moasis.monolithicbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
