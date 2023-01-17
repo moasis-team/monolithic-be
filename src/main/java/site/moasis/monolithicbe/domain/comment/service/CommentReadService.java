bbb47db9663e749dfd7d8c248623b9f596e0764b
sungwon-097 <csw2757@gmail.com>
2023-01-17 11:22:05 +0900

    #8 세부 기능 구현
    조회는 read, 생성과 삭제는 write 서비스에서 정의

--- src/main/java/site/moasis/monolithicbe/domain/comment/service/CommentReadService.java
+++ src/main/java/site/moasis/monolithicbe/domain/comment/service/CommentReadService.java
@@ -1,4 +1,37 @@
 package site.moasis.monolithicbe.domain.comment.service;
 
-public class CommentReadService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.moasis.monolithicbe.common.exception.EntityNotFoundException;
+import site.moasis.monolithicbe.common.response.ErrorCode;
+import site.moasis.monolithicbe.domain.comment.dto.CommentDto;
+import site.moasis.monolithicbe.domain.comment.entity.Comment;
+import site.moasis.monolithicbe.domain.comment.repository.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CommentReadService{
+
+    private final CommentRepository commentRepository;
+
+    public CommentDto.commentOneDto retrieveCommentById(Long commentId){
+        return toDto(Optional.ofNullable(commentRepository.findById(commentId).orElseThrow(EntityNotFoundException::new)));
+    }
+    public CommentDto.commentResponseDto retrieveCommentByArticleId(Long articleId){
+        return toDto(commentRepository.findByArticleId(articleId));
+    }
+    public CommentDto.commentResponseDto retrieveCommentByUserId(Long userId){
+        return toDto(commentRepository.findByUserId(userId));
+    }
+    public CommentDto.commentResponseDto toDto(List<Comment> commentList){
+        return new CommentDto.commentResponseDto(commentList);
+    }
+    public CommentDto.commentOneDto toDto(Optional<Comment> comment){
+        return new CommentDto.commentOneDto(comment);
+    }
 }
--- src/main/java/site/moasis/monolithicbe/domain/comment/service/CommentWriteService.java
+++ src/main/java/site/moasis/monolithicbe/domain/comment/service/CommentWriteService.java
@@ -1,4 +1,31 @@
 package site.moasis.monolithicbe.domain.comment.service;
 
-public class CommentWriteService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.moasis.monolithicbe.common.exception.EntityNotFoundException;
+import site.moasis.monolithicbe.domain.comment.dto.CommentDto;
+import site.moasis.monolithicbe.domain.comment.entity.Comment;
+import site.moasis.monolithicbe.domain.comment.repository.CommentRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CommentWriteService{
+
+    private final CommentRepository commentRepository;
+
+    public void createComment(CommentDto.commentCreateDto commentCreateDto){
+        Comment commentEntity = Comment.builder()
+                .content(commentCreateDto.content())
+                .articleId(commentCreateDto.articleId())
+                .userId(commentCreateDto.userId())
+                .build();
+        commentRepository.save(commentEntity);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId){
+        commentRepository.findById(commentId).orElseThrow(EntityNotFoundException::new);
+        commentRepository.deleteById(commentId);
+    }
 }
