06cbcf21de6f80d88328d5d64f4fa78deaf27c9d
sungwon-097 <csw2757@gmail.com>
2023-01-17 11:23:05 +0900

    #8 Repository에 JPA적용

--- src/main/java/site/moasis/monolithicbe/domain/comment/repository/CommentRepository.java
+++ src/main/java/site/moasis/monolithicbe/domain/comment/repository/CommentRepository.java
@@ -1,4 +1,13 @@
 package site.moasis.monolithicbe.domain.comment.repository;
 
-public class CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNullApi;
+import site.moasis.monolithicbe.domain.comment.entity.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByUserId(Long userId);
 }
