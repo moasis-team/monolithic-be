ce500fb3a6da8e07b2718ab649d308c14deffa41
constwon111 <constwon111@gmail.com>
2023-01-20 20:58:38 +0900

    #10 UserDetailsService implements하여 security에서 필요한 loadUserByUsername를 override함. 또한 authorities가 설정이 안되어 있을경우 에러가 출력되므로 비어있는 리스트로 인자를 넘겨줌.

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserDetailsServiceImpl.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserDetailsServiceImpl.java
@@ -8,23 +8,33 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
 import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserAccountRepository userAccountRepository;
+	private final UserAccountRepository userAccountRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userAccountRepository.findByEmail(email)
-                .map(this::createUserDetails).orElseThrow();
-    }
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		return userAccountRepository.findOneByEmail(email).map(this::createUserDetails).orElseThrow(() -> {
+			throw new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다.");
+		});
+	}
 
-    private UserDetails createUserDetails(UserAccount userAccount) {
-        return User.builder()
-                .username(userAccount.getEmail())
-                .password(userAccount.getPassword())
-                .build();
-    }
+	private UserDetails createUserDetails(UserAccount userAccount) {
+		try {
+			return User.builder()
+					.username(userAccount.getEmail())
+					.password(userAccount.getPassword())
+					.authorities(Collections.emptyList())
+					.build();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
 }
