705145405d982800b5bbf45ac376ce7855a5c1dc
constwon111 <constwon111@gmail.com>
2023-01-20 20:52:11 +0900

    #10 이메일을 고유한 키값으로 하기 위해 unique 설정

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/entity/UserAccount.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/entity/UserAccount.java
@@ -1,9 +1,6 @@
 package site.moasis.monolithicbe.domain.useraccount.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,23 +9,24 @@ import lombok.NoArgsConstructor;
 @Entity
 public class UserAccount {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    private String email;
-    private String password;
-    private String name;
-    private String phoneNumber;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	private String name;
+	private String phoneNumber;
 
-    private UserAccount(String email, String password, String name, String phoneNumber) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
+	private UserAccount(String email, String password, String name, String phoneNumber) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
 
-    public static UserAccount create(String email, String password, String name, String phoneNumber) {
-        return new UserAccount(email, password, name, phoneNumber);
-    }
+	public static UserAccount create(String email, String password, String name, String phoneNumber) {
+		return new UserAccount(email, password, name, phoneNumber);
+	}
 }
