c4781b215becfc4f14f9a3a28c431e9c62ee57f5
constwon111 <constwon111@gmail.com>
2023-01-20 20:45:35 +0900

    #10 jwt 사용을 위한 SecurityConfig의 configure 메서드 오버라이드

--- src/main/java/site/moasis/monolithicbe/application/configuration/SecurityConfig.java
+++ src/main/java/site/moasis/monolithicbe/application/configuration/SecurityConfig.java
@@ -1,19 +1,45 @@
 package site.moasis.monolithicbe.application.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+import site.moasis.monolithicbe.domain.useraccount.JwtAccessDeniedHandler;
+import site.moasis.monolithicbe.domain.useraccount.JwtAuthenticationEntryPoint;
+import site.moasis.monolithicbe.domain.useraccount.TokenProvider;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+	private final CorsFilter corsFilter;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private final TokenProvider tokenProvider;
+
+	// 생성자 통해 스프링 빈 주입받는다.
+	@Autowired
+	public SecurityConfig(
+			TokenProvider tokenProvider,
+			CorsFilter corsFilter,
+			JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+			JwtAccessDeniedHandler jwtAccessDeniedHandler
+	) {
+		this.tokenProvider = tokenProvider;
+		this.corsFilter = corsFilter;
+		this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+		this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,4 +59,40 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
+
+	@Override
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+				// token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+				.csrf().disable()
+
+				.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling()
+				.authenticationEntryPoint(jwtAuthenticationEntryPoint) // 우리가 만든 클래스로 인증 실패 핸들링
+				.accessDeniedHandler(jwtAccessDeniedHandler) // 커스텀 인가 실패 핸들링
+
+
+				// enable h2-console // embedded h2를 위한 설정
+				.and()
+				.headers()
+				.frameOptions()
+				.sameOrigin()
+
+				// 세션을 사용하지 않기 때문에 STATELESS로 설정
+				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+				// api 경로
+				.and().authorizeHttpRequests()
+				.requestMatchers("/users/**").permitAll() // /api/hello
+//				.antMatchers("/api/authenticate").permitAll() // 로그인 경로
+//				.antMatchers("/api/user/signup").permitAll() // 회원가입 경로는 인증없이 호출 가능
+				.anyRequest().authenticated() // 나머지 경로는 jwt 인증 해야함
+
+				.and()
+				.apply(new JwtSecurityConfig(tokenProvider)); // JwtSecurityConfig 적용
+	}
+
+
 }
