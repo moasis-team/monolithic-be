d1b7fc53fb6156ad48f0f7a3815f8f71b7ce88fb
constwon111 <constwon111@gmail.com>
2023-01-20 21:03:56 +0900

    #10 UserAccount의 signIn과 관련된 Controller, Repository, Service 구현

--- src/main/java/site/moasis/monolithicbe/application/controller/UserAccountController.java
+++ src/main/java/site/moasis/monolithicbe/application/controller/UserAccountController.java
@@ -1,11 +1,18 @@
 package site.moasis.monolithicbe.application.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.moasis.monolithicbe.domain.useraccount.ResponseSignIn;
+import site.moasis.monolithicbe.domain.useraccount.JwtFilter;
 import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountJoinRequestDto;
+import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountSignInRequestDto;
 import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
 import site.moasis.monolithicbe.domain.useraccount.service.UserAccountService;
 
@@ -14,10 +21,20 @@ import site.moasis.monolithicbe.domain.useraccount.service.UserAccountService;
 @RestController
 public class UserAccountController {
 
-    private final UserAccountService userAccountService;
+	private final UserAccountService userAccountService;
 
-    @PostMapping
-    public UserAccount join(@RequestBody UserAccountJoinRequestDto userAccountJoinRequestDto) {
-        return userAccountService.create(userAccountJoinRequestDto);
-    }
+
+	@PostMapping("/signin") // Account 인증 API
+	public ResponseEntity<ResponseSignIn> authorize(@Valid @RequestBody UserAccountSignInRequestDto userAccountSignInRequestDto) {
+		ResponseSignIn token = userAccountService.signIn(userAccountSignInRequestDto.email(), userAccountSignInRequestDto.password());
+		// response header 에도 넣고 응답 객체에도 넣는다.
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + token.getAccessToken());
+		return new ResponseEntity<>(token, httpHeaders, HttpStatus.OK);
+	}
+
+	@PostMapping()
+	public UserAccount join(@RequestBody UserAccountJoinRequestDto userAccountJoinRequestDto) {
+		return userAccountService.create(userAccountJoinRequestDto);
+	}
 }
--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountService.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountService.java
@@ -1,9 +1,16 @@
 package site.moasis.monolithicbe.domain.useraccount.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.moasis.monolithicbe.domain.useraccount.ResponseSignIn;
+import site.moasis.monolithicbe.domain.useraccount.TokenProvider;
 import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountJoinRequestDto;
 import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
 import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
@@ -12,17 +19,56 @@ import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountReposit
 @Service
 public class UserAccountService {
 
-    private final UserAccountRepository userAccountRepository;
-    private final PasswordEncoder passwordEncoder;
+	private final UserAccountRepository userAccountRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final TokenProvider tokenProvider;
+	private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    @Transactional
-    public UserAccount create(UserAccountJoinRequestDto userAccountJoinRequestDto) {
-        UserAccount userAccount = UserAccount.create(
-                userAccountJoinRequestDto.email(),
-                passwordEncoder.encode(userAccountJoinRequestDto.password()),
-                userAccountJoinRequestDto.name(),
-                userAccountJoinRequestDto.phoneNumber()
-        );
-        return userAccountRepository.save(userAccount);
-    }
+
+	@Autowired
+	public UserAccountService(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, UserAccountRepository userAccountRepository, PasswordEncoder passwordEncoder) {
+		this.userAccountRepository = userAccountRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.tokenProvider = tokenProvider;
+		this.authenticationManagerBuilder = authenticationManagerBuilder;
+	}
+
+
+	@Transactional
+	public UserAccount create(UserAccountJoinRequestDto userAccountJoinRequestDto) {
+		UserAccount userAccount = UserAccount.create(
+				userAccountJoinRequestDto.email(),
+				passwordEncoder.encode(userAccountJoinRequestDto.password()),
+				userAccountJoinRequestDto.name(),
+				userAccountJoinRequestDto.phoneNumber()
+		);
+
+		return userAccountRepository.save(userAccount);
+	}
+	// username 과 패스워드로 사용자를 인증하여 액세스토큰을 반환한다.
+	@Transactional
+	public ResponseSignIn signIn(String email, String password) {
+		// 받아온 유저네임과 패스워드를 이용해 UsernamePasswordAuthenticationToken 객체 생성
+		UsernamePasswordAuthenticationToken authenticationToken =
+				new UsernamePasswordAuthenticationToken(email, password);
+
+		// authenticationToken 객체를 통해 Authentication 객체 생성
+		// 이 과정에서 CustomUserDetailsService 에서 우리가 재정의한 loadUserByUsername 메서드 호출
+		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+		// 그 객체를 시큐리티 컨텍스트에 저장
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		String accessToken = null;
+		// 인증 정보를 기준으로 jwt access 토큰 생성
+		try {
+			accessToken = tokenProvider.createToken(authentication);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ResponseSignIn.builder()
+				.accessToken(accessToken)
+				.build();
+	}
 }
