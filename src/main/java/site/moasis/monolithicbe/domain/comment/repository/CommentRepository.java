dea8d1e56e2bbebacf09a69d783cf7334a8fa5e3
sungwon-097 <csw2757@gmail.com>
2023-01-22 17:43:34 +0900

    #8 유저별, 게시물별 댓글 삭제가 가능하도록 메소드 정의

--- src/main/java/site/moasis/monolithicbe/domain/comment/repository/CommentRepository.java
+++ src/main/java/site/moasis/monolithicbe/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package site.moasis.monolithicbe.domain.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNullApi;
 import site.moasis.monolithicbe.domain.comment.entity.Comment;
 
 import java.util.List;
@@ -10,4 +9,6 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByArticleId(Long articleId);
     List<Comment> findByUserId(Long userId);
+    void deleteByUserId(Long userId);
+    void deleteByArticleId(Long articleId);
 }
