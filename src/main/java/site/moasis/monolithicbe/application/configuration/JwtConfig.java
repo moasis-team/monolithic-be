7d4bd958df9366aa34720d90e60512e3b758f051
constwon111 <constwon111@gmail.com>
2023-01-24 19:16:48 +0900

    #10 Bean에 name= "tokenProvider" 라고 이름지어준 부분 무의미해서 제거

--- src/main/java/site/moasis/monolithicbe/application/configuration/JwtConfig.java
+++ src/main/java/site/moasis/monolithicbe/application/configuration/JwtConfig.java
@@ -13,7 +13,7 @@ public class JwtConfig {
 	private Long accessTokenValidityInSeconds;
 
 	// 액세스 토큰 발급용, 리프레시 토큰 발급용은 각각 별도의 키와 유효기간을 갖는다.
-	@Bean(name = "tokenProvider")
+	@Bean()
 	public TokenProvider tokenProvider() {
 		return new TokenProvider(accessTokenSecret, accessTokenValidityInSeconds);
 	}
