6d26e824a6c0dc469688765e783eb385e9dc4a9e
constwon111 <constwon111@gmail.com>
2023-01-24 22:12:01 +0900

    #10 service를 read랑 write로 분리

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountReadService.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountReadService.java
@@ -5,54 +5,26 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.moasis.monolithicbe.domain.useraccount.TokenProvider;
-import site.moasis.monolithicbe.domain.useraccount.UserRole;
 import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountDto;
-import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
-import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
-
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
-public class UserAccountService {
-
-	private final UserAccountRepository userAccountRepository;
-	private final PasswordEncoder passwordEncoder;
+public class UserAccountReadService {
 	private final TokenProvider tokenProvider;
 	private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
-	@Transactional
-	public UserAccount signUp(UserAccountDto.UserAccountJoinRequestDto userAccountJoinRequestDto) {
-		UserAccount userAccount = UserAccount.create(
-				userAccountJoinRequestDto.email(),
-				passwordEncoder.encode(userAccountJoinRequestDto.password()),
-				userAccountJoinRequestDto.name(),
-				userAccountJoinRequestDto.phoneNumber(),
-				UserRole.ROLE_USER
-		);
-		return userAccountRepository.save(userAccount);
-	}
-	// username 과 패스워드로 사용자를 인증하여 액세스토큰을 반환한다.
-	@Transactional(readOnly = true)
 	public UserAccountDto.UserAccountSignInResponseDto signIn(String email, String password) {
-		// 받아온 유저네임과 패스워드를 이용해 UsernamePasswordAuthenticationToken 객체 생성
 		UsernamePasswordAuthenticationToken authenticationToken =
 				new UsernamePasswordAuthenticationToken(email, password);
 
-		// authenticationToken 객체를 통해 Authentication 객체 생성
-		// 이 과정에서 CustomUserDetailsService 에서 우리가 재정의한 loadUserByUsername 메서드 호출
 		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-
-		// 그 객체를 시큐리티 컨텍스트에 저장
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-
 		String accessToken = null;
-		// 인증 정보를 기준으로 jwt access 토큰 생성
+
 		try {
 			accessToken = tokenProvider.createToken(authentication);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
