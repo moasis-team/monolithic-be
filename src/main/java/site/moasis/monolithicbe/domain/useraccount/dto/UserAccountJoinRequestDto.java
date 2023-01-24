5e53f798b7a2f431c48768f83b87123f2c7a04f6
constwon111 <constwon111@gmail.com>
2023-01-24 21:04:54 +0900

    #10 UserAccount관련 DTO를 한곳에 합침

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/dto/UserAccountJoinRequestDto.java
+++
@@ -1,8 +0,0 @@
-package site.moasis.monolithicbe.domain.useraccount.dto;
-
-public record UserAccountJoinRequestDto(
-		String email,
-		String password,
-		String name,
-		String phoneNumber) {
-}
--- src/main/java/site/moasis/monolithicbe/domain/useraccount/dto/UserAccountSignInResponseDto.java
+++
@@ -1,9 +0,0 @@
-package site.moasis.monolithicbe.domain.useraccount.dto;
-
-public record UserAccountSignInResponseDto(
-		String accessToken
-) {
-	public static UserAccountSignInResponseDto toDto(String accessToken) {
-		return new UserAccountSignInResponseDto(accessToken);
-	}
-}
