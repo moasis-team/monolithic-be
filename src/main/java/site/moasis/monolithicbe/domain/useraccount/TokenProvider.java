ae7b9baecfa8e910a79e6bc70751cc397e2a9d34
constwon111 <constwon111@gmail.com>
2023-01-24 22:11:36 +0900

    #10 주석제거

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/TokenProvider.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/TokenProvider.java
@@ -17,29 +17,23 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-// 토큰 생성, 검증
 public class TokenProvider {
 
-	protected final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
-
 	protected static final String AUTHORITIES_KEY = "auth";
-
+	protected final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
 	protected final String secret;
 	protected final long tokenValidityInMilliseconds;
-
 	protected Key key;
 
 
 	public TokenProvider(String secret, long tokenValidityInSeconds) {
 		this.secret = secret;
 		this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
-		//시크릿 값을 decode해서 키 변수에 할당
 		byte[] keyBytes = Decoders.BASE64.decode(secret);
 		this.key = Keys.hmacShaKeyFor(keyBytes);
 
 	}
 
-	// 토큰 생성
 	public String createToken(Authentication authentication) {
 		String authorities = authentication.getAuthorities().stream()
 				.map(GrantedAuthority::getAuthority)
@@ -55,7 +49,6 @@ public class TokenProvider {
 				.compact();
 	}
 
-	// 토큰을 받아 클레임을 만들고 권한정보를 빼서 시큐리티 유저객체를 만들어 Authentication 객체 반환
 	public Authentication getAuthentication(String token) {
 
 		Claims claims = Jwts
@@ -70,13 +63,11 @@ public class TokenProvider {
 						.map(SimpleGrantedAuthority::new)
 						.collect(Collectors.toList());
 
-		// 디비를 거치지 않고 토큰에서 값을 꺼내 바로 시큐리티 유저 객체를 만들어 Authentication을 만들어 반환하기에 유저네임, 권한 외 정보는 알 수 없다.
 		User principal = new User(claims.getSubject(), "", authorities);
 
 		return new UsernamePasswordAuthenticationToken(principal, token, authorities);
 	}
 
-	// 토큰 유효성 검사
 	public boolean validateToken(String token) {
 		try {
 			Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
