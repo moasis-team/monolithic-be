9c336c27c2ca7e2ff84f019d10f5676a6591c185
constwon111 <constwon111@gmail.com>
2023-01-24 22:12:40 +0900

    #10 CommonResponse를 사용해 반환하도록 수정

--- src/main/java/site/moasis/monolithicbe/application/controller/UserAccountController.java
+++ src/main/java/site/moasis/monolithicbe/application/controller/UserAccountController.java
@@ -8,24 +8,26 @@ import org.springframework.web.bind.annotation.*;
 import site.moasis.monolithicbe.common.response.CommonResponse;
 import site.moasis.monolithicbe.domain.useraccount.dto.*;
 import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
-import site.moasis.monolithicbe.domain.useraccount.service.UserAccountService;
+import site.moasis.monolithicbe.domain.useraccount.service.UserAccountReadService;
+import site.moasis.monolithicbe.domain.useraccount.service.UserAccountWriteService;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
 public class UserAccountController {
 
-	private final UserAccountService userAccountService;
+	private final UserAccountReadService userAccountReadService;
+	private final UserAccountWriteService userAccountWriteService;
 
-	@PostMapping("/signin") // Account 인증 API
+	@PostMapping("/signin")
 	public ResponseEntity<CommonResponse<?>> authorize(@Valid @RequestBody UserAccountDto.UserAccountSignInRequestDto userAccountSignInRequestDto) {
-		UserAccountDto.UserAccountSignInResponseDto token = userAccountService.signIn(userAccountSignInRequestDto.email(), userAccountSignInRequestDto.password());
+		UserAccountDto.UserAccountSignInResponseDto token = userAccountReadService.signIn(userAccountSignInRequestDto.email(), userAccountSignInRequestDto.password());
 		return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(token, "로그인 완료"));
 	}
 
 	@PostMapping()
 	public ResponseEntity<CommonResponse<?>> join(@RequestBody UserAccountDto.UserAccountJoinRequestDto userAccountJoinRequestDto) {
-		UserAccount savedUserAccount = userAccountService.signUp(userAccountJoinRequestDto);
+		UserAccount savedUserAccount = userAccountWriteService.signUp(userAccountJoinRequestDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success(UserAccountDto.UserAccountJoinResponseDto.toDto(savedUserAccount), "회원가입 완료"));
 	}
 }
