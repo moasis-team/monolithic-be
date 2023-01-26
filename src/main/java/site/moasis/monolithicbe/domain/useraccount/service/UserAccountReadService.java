95098fa0e1dcc5dd262eedc41e59d565338234e1
constwon111 <constwon111@gmail.com>
2023-01-27 01:58:22 +0900

    #17 signIn 메서드 이동 및 리프레시토큰 적용     - 리프레시토큰 userAccount에 저장     - write과정이 들어가므로 readService에서 writeService로 이동     - 바뀐 userAccountSignInResponse에 따른 수정

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountReadService.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountReadService.java
@@ -1,33 +1,12 @@
 package site.moasis.monolithicbe.domain.useraccount.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.moasis.monolithicbe.domain.useraccount.AccessTokenManager;
-import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountDto;
+
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class UserAccountReadService {
-	private final AccessTokenManager accessTokenManager;
-	private final AuthenticationManagerBuilder authenticationManagerBuilder;
-	public UserAccountDto.UserAccountSignInResponseDto signIn(String email, String password) {
-		UsernamePasswordAuthenticationToken authenticationToken =
-				new UsernamePasswordAuthenticationToken(email, password);
 
-		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String accessToken = null;
-
-		try {
-			accessToken = accessTokenManager.createToken(authentication);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return UserAccountDto.UserAccountSignInResponseDto.toDto(accessToken);
-	}
 }
--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountWriteService.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountWriteService.java
@@ -1,30 +1,42 @@
 package site.moasis.monolithicbe.domain.useraccount.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.moasis.monolithicbe.common.exception.BusinessException;
 import site.moasis.monolithicbe.common.exception.ErrorCode;
+import site.moasis.monolithicbe.domain.useraccount.AccessTokenManager;
+import site.moasis.monolithicbe.domain.useraccount.RefreshTokenManager;
 import site.moasis.monolithicbe.domain.useraccount.UserRole;
-import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountDto;
 import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
 import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
+
+import java.util.Optional;
+
+import static site.moasis.monolithicbe.domain.useraccount.dto.UserAccountDto.UserAccountJoinRequestDto;
+import static site.moasis.monolithicbe.domain.useraccount.dto.UserAccountDto.UserAccountSignInResponseDto;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class UserAccountWriteService {
-
+	private final AccessTokenManager accessTokenManager;
 	private final UserAccountRepository userAccountRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final AuthenticationManagerBuilder authenticationManagerBuilder;
+	private final RefreshTokenManager refreshTokenManager;
 
 	private void checkDuplicatedEmail(String email){
 		if(userAccountRepository.existsByEmail(email)){
 			throw new BusinessException(ErrorCode.DUPLICATE);
 		};
 	}
-	public UserAccount signUp(UserAccountDto.UserAccountJoinRequestDto userAccountJoinRequestDto) {
+	public UserAccount signUp(UserAccountJoinRequestDto userAccountJoinRequestDto) {
 		checkDuplicatedEmail(userAccountJoinRequestDto.email());
 
 		UserAccount userAccount = UserAccount.create(
@@ -36,5 +48,28 @@ public class UserAccountWriteService {
 		);
 
 		return userAccountRepository.save(userAccount);
+	}
+
+
+	public UserAccountSignInResponseDto signIn(String email, String password) {
+		UsernamePasswordAuthenticationToken authenticationToken =
+				new UsernamePasswordAuthenticationToken(email, password);
+
+		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		String refreshToken = refreshTokenManager.createToken(authentication);
+		Optional<UserAccount> userAccountOptional = this.userAccountRepository.findByEmail(authentication.getName());
+
+		userAccountOptional.ifPresentOrElse(
+				userAccount -> {
+					userAccount.registerRefreshToken(refreshToken);
+					userAccountRepository.save(userAccount);
+				},
+				() -> {
+					throw new BusinessException(ErrorCode.NOT_FOUND);
+				});
+
+		return UserAccountSignInResponseDto.toDto(accessTokenManager.createToken(authentication), refreshToken);
 	}
 }
