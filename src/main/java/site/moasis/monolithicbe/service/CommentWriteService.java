cf06745616b98dc7aff301df73767f4d4663b37c
sungwon-097 <csw2757@gmail.com>
2023-02-01 01:27:01 +0900

    #38 좋아요 기능 추가
    - Read : 본인이 좋아요 한 댓글인지, 전체 선호 횟수는 몇 번인지 표시
    - Write : 좋아요 또는 취소 기능 구현

--- src/main/java/site/moasis/monolithicbe/service/CommentWriteService.java
+++ src/main/java/site/moasis/monolithicbe/service/CommentWriteService.java
@@ -2,13 +2,20 @@ package site.moasis.monolithicbe.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.moasis.monolithicbe.common.exception.BusinessException;
 import site.moasis.monolithicbe.common.exception.ErrorCode;
 import site.moasis.monolithicbe.domain.comment.Comment;
+import site.moasis.monolithicbe.domain.comment.CommentLikes;
+import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
+import site.moasis.monolithicbe.infrastructure.CommentLikesRepository;
 import site.moasis.monolithicbe.infrastructure.CommentRepository;
 
+import static site.moasis.monolithicbe.service.CommentCommand.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,12 +26,18 @@ import java.util.UUID;
 public class CommentWriteService{
 
     private final CommentRepository commentRepository;
+    private final CommentLikesRepository commentLikesRepository;
+    private final UserAccountRepository userAccountRepository;
 
-    public CommentInfo registerComment(CommentCommand.RegisterCommentCommand registerCommentCommand){
+    public CommentInfo registerComment(RegisterCommentCommand registerCommentCommand){
+
+        UUID userId = userAccountRepository.selectIdByEmail(getUserEmail()).orElseThrow(()->
+                new BusinessException(ErrorCode.UNAUTHORIZED, "user.byEmail", null));
+
         var comment = Comment.builder()
                 .content(registerCommentCommand.getContent())
                 .articleId(registerCommentCommand.getArticleId())
-                .userId(registerCommentCommand.getUserId())
+                .userId(userId)
                 .build();
 
         return CommentInfoMapper.INSTANCE.toCommentInfo(commentRepository.save(comment));
@@ -36,5 +49,27 @@ public class CommentWriteService{
         commentRepository.deleteByCommentId(commentId);
         
         return commentId;
+    }
+
+    public Boolean likeComment(UUID articleId, UUID commentId) {
+        String userEmail = getUserEmail();
+        Comment comment = commentRepository.selectById(commentId).orElseThrow(()->
+                new BusinessException(ErrorCode.NOT_FOUND, "comment.byId", List.of(commentId.toString())));
+        Boolean exist = commentLikesRepository.existsByCommentIdAndCreateBy(commentId, userEmail);
+
+        if (exist)
+            commentLikesRepository.deleteLikes(commentId, userEmail);
+        else
+            commentLikesRepository.save(new CommentLikes(commentId, articleId, userEmail));
+
+        comment.setIsLiked(!exist);
+        return comment.getIsLiked();
+    }
+
+    private String getUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getClass().equals(AnonymousAuthenticationToken.class))
+            throw new BusinessException(ErrorCode.UNAUTHORIZED, "user.byEmail", null);
+        return authentication.getName();
     }
 }
