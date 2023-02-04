976430577d4417c8ac4afc0d3fd84137994a6a15
constwon111 <constwon111@gmail.com>
2023-02-05 03:01:47 +0900

    #31 [Chore] springConfig를 oAuth가 가능하도록 설정 - 네이버 로그인이 성공했을때 인증받은 정보를 처리할 principalOauth2UserService와 연결 - 성공했을때 url을 다음과 같이 설정

--- src/main/java/site/moasis/monolithicbe/configuration/SecurityConfig.java
+++ src/main/java/site/moasis/monolithicbe/configuration/SecurityConfig.java
@@ -4,28 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
+import site.moasis.monolithicbe.domain.useraccount.AccessTokenManager;
 import site.moasis.monolithicbe.domain.useraccount.JwtAccessDeniedHandler;
 import site.moasis.monolithicbe.domain.useraccount.JwtAuthenticationEntryPoint;
-import site.moasis.monolithicbe.domain.useraccount.AccessTokenManager;
+import site.moasis.monolithicbe.domain.useraccount.service.PrincipalOauth2UserService;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+public class SecurityConfig {
 	private final CorsFilter corsFilter;
 	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 	private final AccessTokenManager accessTokenManager;
+	private final PrincipalOauth2UserService principalOauth2UserService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -42,12 +42,19 @@ public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFil
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
 			.and().authorizeHttpRequests()
-			.requestMatchers(HttpMethod.POST,"/users").permitAll()
+			.requestMatchers(HttpMethod.POST, "/users").permitAll()
 			.requestMatchers("/users/signin").permitAll()
+			.requestMatchers("/users/**").permitAll()
 			.anyRequest().permitAll()
 
 			.and()
-			.apply(new JwtSecurityConfig(accessTokenManager));
+			.apply(new JwtSecurityConfig(accessTokenManager))
+
+			.and()
+			.oauth2Login()
+			.defaultSuccessUrl("/users/authorization/success")
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService);
 
 		return http.build();
 	}
