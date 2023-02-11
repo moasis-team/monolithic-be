6937e51dfc6924b9e4b83b9f7bfa558e0e360909
AnTaeWook <antk7894@naver.com>
2023-02-11 15:30:44 +0900

    #59 - 비밀번호 변경 서비스 구현
    로그인 및 비로그인 상태에서 비밀번호를 변경하기 위한
    서비스 로직을 구현하고, 관련 Dto를 추가함
    
    + 사용자 이메일 중복 검사를 읽기 서비스로 이동시켰음.

--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountReadService.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserAccountReadService.java
@@ -3,10 +3,20 @@ package site.moasis.monolithicbe.domain.useraccount.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.moasis.monolithicbe.common.exception.BusinessException;
+import site.moasis.monolithicbe.common.exception.ErrorCode;
+import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class UserAccountReadService {
 
+    private final UserAccountRepository userAccountRepository;
+
+    public void checkDuplicatedEmail(String email){
+        if(userAccountRepository.existsByEmail(email)){
+            throw new BusinessException(ErrorCode.DUPLICATE);
+        };
+    }
 }
--- src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserEmailService.java
+++ src/main/java/site/moasis/monolithicbe/domain/useraccount/service/UserEmailService.java
@@ -6,13 +6,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import site.moasis.monolithicbe.common.exception.BusinessException;
 import site.moasis.monolithicbe.common.exception.ErrorCode;
 import site.moasis.monolithicbe.configuration.AppProperties;
+import site.moasis.monolithicbe.domain.useraccount.dto.UserAccountDto.ResetPasswordRequestDto;
+import site.moasis.monolithicbe.domain.useraccount.entity.UserAccount;
+import site.moasis.monolithicbe.domain.useraccount.repository.UserAccountRepository;
 
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static site.moasis.monolithicbe.common.message.UserMessage.*;
@@ -25,13 +30,20 @@ public class UserEmailService {
     private final JavaMailSender javaMailSender;
     private final AppProperties appProperties;
     private final ConcurrentHashMap<String, Integer> codeMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<UUID, String> urlMap = new ConcurrentHashMap<>();
+    private final UserAccountReadService userAccountReadService;
+    private final UserAccountRepository userAccountRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     public void sendCode(String email) {
+        userAccountReadService.checkDuplicatedEmail(email);
         int code = generateCode();
+
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
-            mimeMessageHelper.setFrom(new InternetAddress(appProperties.getMail().getUsername(), EMAIL_CERTIFICATION_MAIL_SENDER, "UTF-8"));
+            mimeMessageHelper.setFrom(new InternetAddress(appProperties.getMail().getUsername(), MAIL_SENDER, "UTF-8"));
             mimeMessageHelper.setTo(email);
             mimeMessageHelper.setSubject(EMAIL_CERTIFICATION_MAIL_TITLE);
             mimeMessageHelper.setText(EMAIL_CERTIFICATION_MAIL_CONTENT_PREFIX + code + EMAIL_CERTIFICATION_MAIL_CONTENT_POSTFIX, true);
@@ -43,10 +55,45 @@ public class UserEmailService {
         codeMap.put(email, code);
     }
 
+    public void sendResetUrl(ResetPasswordRequestDto resetPasswordRequestDto) {
+        UserAccount userAccount = userAccountRepository.findByEmail(resetPasswordRequestDto.email()).orElseThrow();
+        if (!userAccount.getPhoneNumber().equals(resetPasswordRequestDto.phoneNumber())) {
+            throw new BusinessException(ErrorCode.ACCOUNT_MISMATCH);
+        }
+
+        UUID uuid = UUID.randomUUID();
+        String url = "https://www.moasis.site/users/unauthorized-password" + uuid;
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            mimeMessageHelper.setFrom(new InternetAddress(appProperties.getMail().getUsername(), MAIL_SENDER, "UTF-8"));
+            mimeMessageHelper.setTo(resetPasswordRequestDto.email());
+            mimeMessageHelper.setSubject(PASSWORD_RESET_MAIL_TITLE);
+            mimeMessageHelper.setText(PASSWORD_RESET_MAIL_CONTENT_PREFIX + url + PASSWORD_RESET_MAIL_CONTENT_POSTFIX, true);
+            javaMailSender.send(mimeMessage);
+        } catch (Exception e) {
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+        log.info("사용자 이메일={}, 발급된 url={}", resetPasswordRequestDto.email(), uuid);
+        urlMap.put(uuid, resetPasswordRequestDto.email());
+    }
+
     public void certificate(String email, int code) {
         if (!codeMap.get(email).equals(code)) {
             throw new BusinessException(ErrorCode.CODE_MISMATCH, "user.byCode", List.of(email));
         }
+        codeMap.remove(email);
+    }
+
+    public void certificateResetUrl(UUID url, String email) {
+        if (!urlMap.get(url).equals(email)) {
+            throw new BusinessException(ErrorCode.ACCOUNT_MISMATCH, "user.byCode", List.of(email));
+        }
+        urlMap.remove(url);
+    }
+
+    public void resetPassword() {
+
     }
 
     private int generateCode() {
